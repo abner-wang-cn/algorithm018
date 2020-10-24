78d0068db20023fc39d9efd5efcc34ecf60e66f1
Abner Wang <abnerwang@Abners-MacBook-Pro.local>
2020-10-24 16:19:45 +0800

    fix

--- week01/PlusOne.java
+++ week01/PlusOne.java
@@ -1,8 +1,11 @@
 /**
  * 加一
- * 这个解法其实没有充分利用+1这个条件，加任何小于10的数都适用
+ *
  * */
 public class PlusOne{
+/**
+ * 这个解法其实没有充分利用+1这个条件，加任何小于10的数都适用
+ * */
     public int[] plusOne(int[] digits) {
         int n = digits.length;
         int acc = 0;
@@ -35,5 +38,24 @@ public class PlusOne{
             }
             return res;
         }
+    }
+
+    /**
+     *  比较简单巧妙的解法
+     * */
+    public int[] plusOneNew(int[] digits) {
+        int n =digits.length;
+        for(int i=n-1;i>=0;i--){
+            digits[i]++;
+            digits[i]=digits[i]%10;
+            if(digits[i]>0){
+                break;
+            }
+        }
+        if(digits[0]==0){
+            digits = new int[n+1];
+            digits[0]=1;
+        }
+        return digits;
     }
 }

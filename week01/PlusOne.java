d0b1a9c936ff30b7b37aa5ed44435df7a16a16a9
Abner Wang <abnerwang@Abners-MacBook-Pro.local>
2020-10-24 16:01:42 +0800

    加一

---
+++ week01/PlusOne.java
@@ -0,0 +1,38 @@
+/**
+ * 加一
+ * */
+public class PlusOne{
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int acc = 0;
+
+        if(digits[n-1]+1<10){
+            digits[n-1]=digits[n-1]+1;
+            return digits;
+        }else{
+            digits[n-1]=(digits[n-1]+1)%10;
+        }
+        acc=1;
+        for(int i=n-2;i>=0;i--){
+            if(acc==0) {
+                break;
+            }
+            if(digits[i]+1>=10){
+                digits[i]=(digits[i]+1)%10;
+            }else{
+                digits[i]=digits[i]+1;
+                acc=0;
+            }
+        }
+        if(acc==0){
+            return digits;
+        }else{
+            int[] res = new int[n+1];
+            res[0]=1;
+            for(int i=0;i<n;i++){
+                res[i+1]=digits[i];
+            }
+            return res;
+        }
+    }
+}
